f269b395fa32ec8530ba55beeaf733a911c57c23
Stefan Scepanovic <scepa91@icloud.com>
2019-09-16 18:01:24 -0500

    i have links for icons

--- portfolio/src/components/Main.js
+++ portfolio/src/components/Main.js
@@ -1,6 +1,7 @@
 import React, { Component } from "react";
 import "../App.css";
 import { fallDown as Menu } from "react-burger-menu";
+import { FaInstagram, FaGithub, FaLinkedin, FaSlack } from "react-icons/fa";
 
 import picture from "../images/Stefan.jpeg";
 
@@ -37,6 +38,60 @@ class Main extends Component {
             </a>
           </Menu>
         </div>
+        <a href="https://github.com/scepa991" target="_blank">
+          <FaGithub
+            style={{
+              color: "black",
+              width: "3vw",
+              height: "3vw",
+              position: "absolute",
+              top: "1%",
+              left: "45%"
+            }}
+          />
+        </a>
+
+        <a href="https://instagram.com/scepa991/" target="_blank">
+          <FaInstagram
+            style={{
+              color: "#d3d3d3",
+              width: "3vw",
+              height: "3vw",
+              position: "absolute",
+              top: "1%",
+              left: "51%"
+            }}
+          />
+        </a>
+        <a href="https://linkedin.com/in/stefanscepanovic/" target="_blank">
+          <FaLinkedin
+            style={{
+              color: "black",
+              width: "3vw",
+              height: "3vw",
+              position: "absolute",
+              top: "1%",
+              left: "40%"
+            }}
+          />
+        </a>
+        <FaSlack
+          style={{
+            color: "d3d3d3",
+            width: "3vw",
+            height: "3vw",
+            position: "absolute",
+            top: "1%",
+            left: "56%"
+          }}
+        />
+        <p className="quote">
+          “I'm not a great programmer;
+          <br />
+          <br />
+          I'm just a good programmer with great habits.” <br />
+          <br />― Kent Beck
+        </p>
 
         <img className="img" src={picture} alt={"Stefan image"} />
       </div>
