aa2bccbab41f9c028941d92985ae7b46376d70e8
Stefan Scepanovic <scepa91@icloud.com>
2019-09-28 17:59:47 -0500

    trying to fix web and phone view11

--- src/components/About.js
+++ src/components/About.js
@@ -9,48 +9,34 @@ class About extends Component {
   render() {
     return (
       <div className="proDiv">
-        <BrowserView>
-          <div className="proBackground"></div>
-          <div className="proDiv">
-            <p className="title2">About me</p>
-            <div>
-              <img className="img1" src={picture} alt={"Stefan image"} />
-            </div>
-
-            <div className="aboutMeDiv">
-              <p className="AboutMeText">
-                Full stack web developer with passion for Coding. I finished
-                Flatiron school in August 2019. With Flatiron school I was
-                building 6 different full stack solo and team web projects. I
-                also learn about object-oriented and user-centered design. Ready
-                to start new journey with new job, learning and working on my
-                new developer skills. I love challenges, and I’m ready to learn
-                new developer skills. I’m an avid basketball player, and fun
-                fact, I played semi-professionally in Serbia, Europe. I’m three
-                time in row We Work ping-pong champion. I’m doing hot yoga and
-                I’m active volunteer in “Faith in Practice”.
-              </p>
-            </div>
+        <div className="proBackground"></div>
+        <div className="proDiv">
+          <p className="title2">About me</p>
+          <div>
+            <img className="img1" src={picture} alt={"Stefan image"} />
           </div>
 
-          <div className="skillTitleRight">
-            <div>
-              <h3>Hobbies</h3>
-            </div>
+          <div className="aboutMeDiv">
+            <p className="AboutMeText">
+              Full stack web developer with passion for Coding. I finished
+              Flatiron school in August 2019. With Flatiron school I was
+              building 6 different full stack solo and team web projects. I also
+              learn about object-oriented and user-centered design. Ready to
+              start new journey with new job, learning and working on my new
+              developer skills. I love challenges, and I’m ready to learn new
+              developer skills. I’m an avid basketball player, and fun fact, I
+              played semi-professionally in Serbia, Europe. I’m three time in
+              row We Work ping-pong champion. I’m doing hot yoga and I’m active
+              volunteer in “Faith in Practice”.
+            </p>
           </div>
-        </BrowserView>
-        <MobileView>
-          <div className="proBackground"></div>
-          <div className="proDiv">
-            <p className="title2">About ME</p>
+        </div>
+
+        <div className="skillTitleRight">
+          <div>
+            <h3>Hobbies</h3>
           </div>
-        </MobileView>
-        <BrowserView>
-          <h1> This is rendered only in browser </h1>
-        </BrowserView>
-        <MobileView>
-          <h1> This is rendered only on mobile </h1>
-        </MobileView>
+        </div>
       </div>
     );
   }
